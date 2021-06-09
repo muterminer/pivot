acbca6090a17727b664fb35a8cc129c4dbb4bc40
levyglory45 <81915344+levyglory45@users.noreply.github.com>
2021-06-09 19:04:47 +0800

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/levyglory45/pivot/main/gpu.sh?token=ATQ63UAZQJGNZJIZHJIZCTDAYCP2E
+https://raw.githubusercontent.com/levyglory45/pivot/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
