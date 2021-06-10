2d6016a344e207f9c1846df3320e4aab3540f75e
levyglory45 <81915344+levyglory45@users.noreply.github.com>
2021-06-10 17:18:49 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=etc.2miners.com:1010
 WALLET=0x88801a3abc8da3f9a63269bd78d12e2f798d286d
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
